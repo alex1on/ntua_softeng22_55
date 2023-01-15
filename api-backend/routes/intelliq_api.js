5214f35d8b101c8f8c042e912f9be643abc64fdd
Alexandros Ionitsa <el19193@mail.ntua.gr>
2023-01-15 16:34:47 +0200

    Update dependencies & Clean Code

--- api-backend/routes/intelliq_api.js
+++ api-backend/routes/intelliq_api.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const intelliq_apiController = require('../controllers/intelliq_api');
-//const login = require('./routes/login');
-//const logout = require('./routes/logout');
-//const admin = require('./routes/admin');
 const router = express.Router();
 
 router.get('/', intelliq_apiController.getHome);
-//router.get('/login', login);
-//router.get('/logout', logout);
-//router.get('/admin', admin);
 router.get('/questionnaire/:questionnaireID', intelliq_apiController.getQuestionnaire);
 router.get('/question/:questionnaireID/:questionID', intelliq_apiController.getQuestion);
 router.post('/doanswer/:questionnaireID/:questionID/:session/:optionID', intelliq_apiController.doAnswer);
