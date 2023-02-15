fafcdfa6cfb29ed053b380217a128c88b2a1cd2a
Alexandros Ionitsa <el19193@mail.ntua.gr>
2023-02-15 12:34:40 +0200

    Update questionnaire_upd.js
    Terminal function that provides the API with a .json file that contains the to be created questionnaire.

--- cli/commands/questionnaire_upd.js
+++ cli/commands/questionnaire_upd.js
@@ -1,18 +1,53 @@
 const chalk = require("chalk");
 const fs = require("fs");
+const request = require("request");
+const FormData = require("form-data");
 
 function questionnaire_upd({ source }) {
   try {
-    let rawdata = fs.readFileSync(source);
-    let jsonObj = JSON.parse(rawdata);
+    if (!fs.existsSync(`${source}`)) {
+      console.error("File does not exist.");
+      return;
+    }
 
-    console.log(jsonObj)
+    var jsonData;
 
-    //   pass arguments to API request
+    var file = fs.readFile(`${source}`, (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
 
-    console.log(
-      chalk.greenBright("\nYou successfully uploaded a new questionnaire!")
-    );
+      jsonData = JSON.parse(data);
+      // In order to send the data via multipart/form-data of 
+      // the http message we have to firstly stringify the 
+      // transmitted data. Note: The transmitted data MUST 
+      // be an object. 
+      jsonString = JSON.stringify(jsonData);
+
+      var form = {
+        file: jsonString
+      }
+
+      request.post({
+        url: "https://localhost:9103/intelliq_api/admin/questionnaire_upd",
+        formData: form,
+        strictSSL: false
+      },
+
+        (err, res, body) => {
+          if (err) {
+            return console.error(err);
+          }
+          console.log(body);
+          console.log(
+            chalk.greenBright("\nYou successfully uploaded a new questionnaire!")
+          );
+        }
+      );
+
+    });
+
   } catch (err) {
     console.error(err)
   }
