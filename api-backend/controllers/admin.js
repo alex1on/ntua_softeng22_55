eff82f650a9acd9b0c79a72c17177197e23065c2
Alexandros Ionitsa <el19193@mail.ntua.gr>
2023-02-15 17:01:07 +0200

    Added endpoint questionnaire creation
    Creates a questionnaire from the .json file that is sent from an http POST request.

--- api-backend/controllers/admin.js
+++ api-backend/controllers/admin.js
@@ -25,8 +25,51 @@ exports.getHealthCheck = (req, res, next) => {
 }
 
 exports.postQuestionnaire_upd = (req, res, next) => {
+    // The questionnaire json data is on the request body as previously 
+    // added by the multer muddleware (for more info check the admin.js route)
+    // The body has to firstly be stringified and afterwards parsed as json. 
+    // The questionnaire data is located in the 'file' field of the json object
+    // which also needs to be parsed as json to correctly show the questionnaire 
+    // data.
+    const file = JSON.parse(JSON.stringify(req.body));
+    const obj = JSON.parse(file.file);
+    // TODO: Check if the .json format corresponds to a correct questionnaire
+    // .json format. 
     pool.getConnection((err, conn) => {
-        // TODO: IMPORT JSON QUESTIONNAIRE FILE
+        var questionnaire_creat = `INSERT INTO Questionnaire (QuestionnaireID, QuestionnaireTitle, UserID) VALUE (?, ?, ?)`;
+        conn.promise().query(questionnaire_creat, [obj.questionnaireID, obj.questionnaireTitle, 3]);
+        // TODO: Specify which user creates the questionnaire
+        var i = 0;
+        while (obj.questions[i] != null) {
+            // Firstly parse the questions and add them to the DB. 
+            // In this way, all questions exist before adding their option, 
+            // it overcomes the issue where the next qID specified by an 
+            // option doesn't exist in the DB.
+            var question_creat = `INSERT INTO Question (QuestionID, QText, Q_Required, Q_Type, QuestionnaireID) VALUE (?, ?, ?, ?, ?)`;
+            // console.log(obj.questions[i].qID, obj.questions[i].qtext, obj.questions[i].required, obj.questions[i].type, obj.questionnaireID);
+            conn.promise().query(question_creat, [obj.questions[i].qID, obj.questions[i].qtext, obj.questions[i].required, obj.questions[i].type, obj.questionnaireID]);
+            i++;
+        }
+        var i = 0;
+        while (obj.questions[i] != null) {
+            var j = 0;
+            while (obj.questions[i].options[j] != null) {
+                // After all the questions have been added to the DB 
+                // the options of each question are added. 
+                var option_creat = `INSERT INTO Q_Option (OptionID, OptText, NextQID, QuestionID, QuestionnaireID) VALUE (?, ?, ?, ?, ?)`;
+                if (obj.questions[i].options[j].nextqID == '-')
+                    obj.questions[i].options[j].nextqID = null;
+                conn.promise().query(option_creat, [obj.questions[i].options[j].optID, obj.questions[i].options[j].opttxt, obj.questions[i].options[j].nextqID, obj.questions[i].qID, obj.questionnaireID]);
+
+                // console.log(obj.questions[i].options[j].optID, obj.questions[i].options[j].opttxt, obj.questions[i].options[j].nextqID, obj.questions[i].qID, obj.questionnaireID)
+                j++;
+            }
+            i++;
+        }
+
+        res.status(200).json({
+            status: 'OK',
+        })
     })
 }
 
